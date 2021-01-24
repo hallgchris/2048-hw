9e852a1403457c7eeebba234d96d52c50a300780
Christopher Hall <hallgchris@gmail.com>
2021-01-25 00:31:23 +1100

    fw: Add function to set random tile to 2 or 4

--- firmware/firmware/src/main.rs
+++ firmware/firmware/src/main.rs
@@ -1,23 +1,23 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+// logs messages to the host stderr; requires a debugger
+use panic_semihosting as _;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
 
-use mmxlviii;
+use mmxlviii::board::Board;
 
 #[entry]
 fn main() -> ! {
-    let test: i32 = mmxlviii::add_one(1);
-
     hprintln!("Hello, world!").unwrap();
-    hprintln!("One plus one is {}", test).unwrap();
+
+    let mut board = Board::empty();
+    for _ in 0..3 {
+        board.set_random();
+    }
+    hprintln!("The current board is {:?}", board.get_board()).unwrap();
 
     debug::exit(debug::EXIT_SUCCESS);
 
