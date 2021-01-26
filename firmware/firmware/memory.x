50d85c8b073f7dc32ad149dd6488fa82ca693540
Christopher Hall <hallgchris@gmail.com>
2021-01-26 12:19:24 +1100

    fw: Implement blinky!

--- firmware/firmware/memory.x
+++ firmware/firmware/memory.x
@@ -3,8 +3,10 @@ MEMORY
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
   /* TODO Adjust these memory regions to match your device memory layout */
   /* These values correspond to the LM3S6965, one of the few devices QEMU can emulate */
+  /*
   FLASH : ORIGIN = 0x00000000, LENGTH = 256K
   RAM : ORIGIN = 0x20000000, LENGTH = 64K
+  */
 
   /* F3DISCOVERY */
   /*
@@ -13,10 +15,8 @@ MEMORY
   */
 
   /* STM32F303x6/x8 */
-  /*
   FLASH : ORIGIN = 0x08000000, LENGTH = 64K
   RAM : ORIGIN = 0x20000000, LENGTH = 12K 
-  */
   /* TODO: There's also a 4 KiB CCM RAM at 0x10000000 */
 }
 
--- firmware/firmware/src/main.rs
+++ firmware/firmware/src/main.rs
@@ -1,25 +1,37 @@
 #![no_std]
 #![no_main]
 
-// logs messages to the host stderr; requires a debugger
-use panic_semihosting as _;
+use panic_halt as _;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+
+use stm32f3xx_hal::{delay, pac, prelude::*};
 
 use mmxlviii::board::Board;
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut status_led = gpioa
+        .pa3
+        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let mut delay = delay::Delay::new(cp.SYST, clocks);
 
     let mut board = Board::empty();
     for _ in 0..3 {
         board.set_random();
     }
-    hprintln!("The current board is {:?}", board.get_board()).unwrap();
 
-    debug::exit(debug::EXIT_SUCCESS);
-
-    loop {}
+    loop {
+        status_led.toggle().unwrap();
+        delay.delay_ms(500u16);
+    }
 }
